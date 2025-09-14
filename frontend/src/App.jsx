990e14140c4fb52f7d51f08e9f4b18bf535e4e99
Rashid-123 <shadanrashid786@gmail.com>
2025-09-14 22:11:18 +0530

    error resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './context/authContext';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
